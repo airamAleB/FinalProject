03702e16ce5bce93c290830f702e8a99db8c4882
Alexandra B <malexandrabaroga@gmail.com>
2023-06-09 11:39:46 +0300

    Initial commit

---
+++ src/main/java/pages/BasePage.java
@@ -0,0 +1,28 @@
+package pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
+
+public class BasePage {
+    protected WebDriver driver;
+    private String baseUrl = " link-ul site-ului ";
+    public BasePage() {
+        // Default constructor
+    }
+    public BasePage(WebDriver driver) {
+        this.driver = driver;
+    }
+    @BeforeClass
+    public void setUp() {
+        System.setProperty("web-driver.chrome.driver", "C:\\Users\\Alexandra\\ChromeDriver\\chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.get(baseUrl);
+    }
+    @AfterMethod //we use @AfterMethod to close the browser after each test case
+    public void tearDown() {
+        driver.quit();
+    }
+}
